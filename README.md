ae82f95acfffadc930d027930c105243d9b5ed49
Jay Swan <jayswan@github.com>
2018-05-08 14:12:52 -0600

    add defang/refang

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
+# qu
 Handle data quotation and spacing in the clipboard.
+
+I constantly need to transform data in the clipboard between various formats:
+
+- SQL (single quoted or unquoted comma-separated)
+- newline (copied from shell stdout, email, etc.)
+- Python printed objects (such as single-quoted lists of strings)
+- Elasticsearch query strings (space separated unquoted, or double quoted JSON style lists)
+- many others
+
+I got tired of doing this with chains of shell utilities, so `qu` is a simple script that reads from STDIN, tries to infer the input format, and outputs the desired format according to the CLI switches.
+
+## Installation
+- Clone this repo.
+- `cp qu.py qu`
+- `chmod +x qu`
+- `mv qu <wherever in your path you put your local hacks>`
+  - I use `/usr/local/bin` for local hack-scripts but people have Opinions on this, so do what you like.
+
+## Inputs
+- newline delimted (commas stripped)
+- comma delimted
+- space delimted
+
+## Outputs
+- single quoted
+- double quoted
+- unquoted
+- comma delimted
+- space delimited
+- newline delimted
+- Splunk "OR" formatting
+
+## Examples
+```
+$ qu -h
+usage: qu [-h] [-c] [-s] [-n] [-q1] [-q2] [-k]
+
+Handle quoting and spacing from STDIN for use with clipboard.
+
+optional arguments:
+  -h, --help           show this help message and exit
+  -c, --comma          output comma delimited
+  -s, --space          output space delimited
+  -n, --newline        output newline delimited
+  -q1, --single-quote  output single quoted
+  -q2, --double-quote  output double quoted
+  -k, --splunk-or      output Splunk OR style
+```
+
+```
+12:54 $ pbpaste
+foo bar baz
+✔ /tmp
+12:54 $ pbpaste | qu -n
+foo
+bar
+baz
+✔ /tmp
+12:54 $ pbpaste | qu -k
+"foo" OR "bar" OR "baz"
+✔ /tmp
+12:54 $ pbpaste | qu -q1 -c
+'foo','bar','baz'
+```
--- qu.py
+++ qu.py
@@ -2,22 +2,30 @@
 from __future__ import print_function
 import argparse
 import sys
+try:
+    from urllib.parse import urlparse,urlunparse
+except ImportError:
+    from urlparse import urlparse
+    from urlparse import urlunparse
 
-def main():
-    parser = argparse.ArgumentParser(description='Handle quoting and spacing from STDIN for use with clipboard.')
-    parser.add_argument('-c','--comma',action='store_true',help='output comma delimited')
-    parser.add_argument('-s','--space',action='store_true',help='output space delimited')
-    parser.add_argument('-n','--newline',action='store_true',help='output newline delimited')
-    parser.add_argument('-q1','--single-quote',action='store_true',help='output single quoted')
-    parser.add_argument('-q2','--double-quote',action='store_true',help='output double quoted')
-    parser.add_argument('-k','--splunk-or',action='store_true',help='output Splunk OR style')
-    args = parser.parse_args()
+def strip_chars(s,strip_chars=["'",'"']):
+    for c in strip_chars:
+        s = s.replace(c,'')
+    return s
 
-    input = sys.stdin.read().strip()
-    quote_chars =["'",'"']
-    for c in quote_chars:
-        input = input.replace(c,'')
+def defang(url):
+    # unclose URL domain components in []
+    parsed = urlparse(url)
+    defanged_netloc = parsed.netloc.replace('.','[.]')
+    return urlunparse([parsed[0],defanged_netloc,parsed[2],parsed[3],parsed[4],parsed[5]])
 
+def refang(url,strip_chars=['[',']']):
+    # strip [] from a string, naively refanging defanged URLs
+    for c in strip_chars:
+        url = url.replace(c,'')
+    return url
+
+def get_tokens(input):
     # newline delimited
     if '\n' in input:
         # if it's newline delimited and has commas, strip commas
@@ -30,6 +38,36 @@ def main():
     # space delimited
     else:
         tokens = [i.strip() for i in input.split(' ')]
+
+
+    return tokens
+
+def main():
+    parser = argparse.ArgumentParser(description='Handle quoting and spacing from STDIN for use with clipboard.')
+    parser.add_argument('-c','--comma',action='store_true',help='output comma delimited')
+    parser.add_argument('-s','--space',action='store_true',help='output space delimited')
+    parser.add_argument('-n','--newline',action='store_true',help='output newline delimited')
+    parser.add_argument('-q1','--single-quote',action='store_true',help='output single quoted')
+    parser.add_argument('-q2','--double-quote',action='store_true',help='output double quoted')
+    parser.add_argument('-k','--splunk-or',action='store_true',help='output Splunk OR style')
+    parser.add_argument('-d','--defang',action='store_true',help='naively defang URL tokens')
+    parser.add_argument('-r','--refang',action='store_true',help='naively refang URL tokens')
+    parser.add_argument('-e','--extra-strip-chars',help='extra chars to strip')
+    args = parser.parse_args()
+
+    input = sys.stdin.read().strip()
+    input = strip_chars(input)
+
+    tokens = get_tokens(input)
+
+    if args.extra_strip_chars:
+        tokens = [strip_chars(token,strip_chars=args.extra_strip_chars).strip() for token in tokens]
+
+    if args.defang:
+        tokens = [defang(token) for token in tokens if 'http' in token]
+
+    if args.refang:
+        tokens = [refang(token) for token in tokens if 'http' in token]
 
     if args.single_quote:
         tokens = ["'{}'".format(i) for i in tokens]
